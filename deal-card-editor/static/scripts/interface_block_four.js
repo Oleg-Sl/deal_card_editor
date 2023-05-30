dd9689d39d82fd797dd07afabf59a4152a9810b0
Oleg <slepcov_oleg@mail.ru>
2023-05-30 23:46:12 +0700

    Changing the editor format to plaintext

--- deal-card-editor/static/scripts/interface_block_four.js
+++ deal-card-editor/static/scripts/interface_block_four.js
@@ -1,63 +1,102 @@
+// const DESC_ORDER = "UF_CRM_1655918107";
+
+// export default class InterfaceBlockFour {
+//     constructor(container, bx24) {
+//         this.container = container;
+//         this.bx24 = bx24;
+//     }
+
+    
+
+//     init() {
+//         // Создание экземпляра ResizeObserver
+//         let resizeObserver = new ResizeObserver(function(entries) {
+//             // for (var entry of entries) {
+//             //     BX24.fitWindow(); 
+//             // }
+//             BX24.fitWindow();
+//         });
+        
+//         // Добавление блока для отслеживания изменения высоты
+//         // resizeObserver.observe(this.container);
+  
+//         // this.container.addEventListener('resize', function() {
+//         //     console.log("resize");
+//         //     BX24.fitWindow();
+//         // });
+//         // this.container.addEventListener('input', function() {
+//         //     console.log("input");
+//         //     BX24.fitWindow();
+//         // });
+//         // $(document.body).bind("DOMSubtreeModified", (e) => {
+//         //     console.log("DOMSubtreeModified");
+//         //     BX24.fitWindow();
+//         //     console.log("AFTER");
+//         // })
+//     }
+
+//     getData() {
+//         let textarea = document.querySelector('#editor');
+//         let editor = sceditor.instance(textarea);
+//         let data = {};
+//         data[DESC_ORDER] = this.truncateStr(editor.val());
+//         return data;
+//     }
+
+//     truncateStr(str) {
+//         return str.replace(/(<p(?:\s[^>]*)?>\s*<br>\s*<\/p>)$/i, "");
+//         // if (str.endsWith("<p><br></p>")) {
+//         //     return str.slice(0, str.length - "<p><br></p>".length);
+//         // }
+//         // return str;
+//     }
+
+//     async render(fields, data) {
+//         let descOrder = data[DESC_ORDER];
+//         let textarea = document.querySelector('#editor');
+//         let editor = sceditor.instance(textarea);
+//         editor.insert(descOrder);
+//         editor.updateOriginal();
+
+//     }
+
+//     // convertingStrToASCII(str) {
+//     //     return str.replace("<p>", "").replace("</p>", "\n");
+//     // }
+
+//     // convertingStrToHTML(str) {
+//     //     return str.replace("<p>", "").replace("</p>", "\n");
+//     // }
+
+// }
+
+
+
+
+
 const DESC_ORDER = "UF_CRM_1655918107";
+
 
 export default class InterfaceBlockFour {
     constructor(container, bx24) {
         this.container = container;
         this.bx24 = bx24;
+        this.quill = null;
     }
 
-    
-
     init() {
-        // Создание экземпляра ResizeObserver
-        let resizeObserver = new ResizeObserver(function(entries) {
-            // for (var entry of entries) {
-            //     BX24.fitWindow(); 
-            // }
-            BX24.fitWindow();
-        });
-        
-        // Добавление блока для отслеживания изменения высоты
-        // resizeObserver.observe(this.container);
-  
-        // this.container.addEventListener('resize', function() {
-        //     console.log("resize");
-        //     BX24.fitWindow();
-        // });
-        // this.container.addEventListener('input', function() {
-        //     console.log("input");
-        //     BX24.fitWindow();
-        // });
-        // $(document.body).bind("DOMSubtreeModified", (e) => {
-        //     console.log("DOMSubtreeModified");
-        //     BX24.fitWindow();
-        //     console.log("AFTER");
-        // })
+        this.quill = new Quill('#editor', {theme: 'snow'});
     }
 
     getData() {
-        let textarea = document.querySelector('#editor');
-        let editor = sceditor.instance(textarea);
-        let data = {};
-        data[DESC_ORDER] = this.truncateStr(editor.val());
+        // let contents = this.quill.getContents();
+        let contents = this.quill.getText();
+        data[DESC_ORDER] = contents;
         return data;
-    }
-
-    truncateStr(str) {
-        return str.replace(/(<p(?:\s[^>]*)?>\s*<br>\s*<\/p>)$/i, "");
-        // if (str.endsWith("<p><br></p>")) {
-        //     return str.slice(0, str.length - "<p><br></p>".length);
-        // }
-        // return str;
     }
 
     async render(fields, data) {
         let descOrder = data[DESC_ORDER];
-        let textarea = document.querySelector('#editor');
-        let editor = sceditor.instance(textarea);
-        editor.insert(descOrder);
-        editor.updateOriginal();
-
+        this.quill.setText(descOrder);
     }
-
 }
