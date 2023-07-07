48598fe47b194eadc42f6ccf4302fc0567ce6992
Oleg <slepcov_oleg@mail.ru>
2023-07-07 12:48:34 +0700

    Changed getting oservers

--- deal-card-editor/static/scripts/utils/task_update.js
+++ deal-card-editor/static/scripts/utils/task_update.js
@@ -40,7 +40,7 @@ async function getDescription(dataDealNew, dataDealOld, dataProducts, fields) {
     // ${getValidData(dataDealNew.UF_CRM_1655918107 || "")}
     // ${getValidData(dataDealNew.UF_CRM_1687857777 || "")}
     let desc = `
-${getValidData(dataDealOld.UF_CRM_1655918107 || "")}
+${getValidData(dataDealNew.UF_CRM_1655918107 || "")}
 ____________
 Согласно ЦП:${getValueByKey(fields.UF_CRM_1640199620.items, dataDealNew.UF_CRM_1640199620)}
 Нужен Замер: ${getValueByKey(fields.UF_CRM_1619441905773.items, dataDealNew.UF_CRM_1619441905773)}
