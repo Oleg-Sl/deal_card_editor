b6fd42cbba3a4ed1dc5ec09e955d1225a051e52a
Oleg <slepcov_oleg@mail.ru>
2023-09-27 00:03:15 +0700

    Changed description task

--- deal-card-editor/static/scripts/interface_block_two.js
+++ deal-card-editor/static/scripts/interface_block_two.js
@@ -35,7 +35,7 @@ export class InterfaceBlockTwo {
         contentHTML += this.getSelectHTML(fields.UF_CRM_1695664525.items, data.UF_CRM_1695664525, "UF_CRM_1695664525", "Замер", 1);  // isMeasuring
         contentHTML += this.getSelectHTML(fields.UF_CRM_1657651541.items, data.UF_CRM_1657651541, "UF_CRM_1657651541", "Демонтаж", 1);   // isDismantling
         contentHTML += this.getSelectHTML(fields.UF_CRM_1637861351.items, data.UF_CRM_1637861351, "UF_CRM_1637861351", "Парковка", 1);   // isParking
-        contentHTML += this.getSelectHTML(fields.UF_CRM_1640199620.items, data.UF_CRM_1640199620, "UF_CRM_1640199620", "Печать <br>согласно ЦП?", 1);
+        contentHTML += this.getSelectHTML(fields.UF_CRM_1640199620.items, data.UF_CRM_1640199620, "UF_CRM_1640199620", "Печать согласно ЦП?", 1);
         // !!! Неизвестно поле !!!
         contentHTML += this.getSelectHTML(fields.UF_CRM_1637861029.items, data.UF_CRM_1637861029, "UF_CRM_1637861029", "Монтаж 24/7", 1);    // isMounting24
         contentHTML += this.getSelectHTML(fields.UF_CRM_1637326777.items, data.UF_CRM_1637326777, "UF_CRM_1637326777", "Наши реквизиты", 2); // ourRequisite
