d3e5c706ff26d9a3fc3ff6bc3dadc98d1935da76
Oleg <slepcov_oleg@mail.ru>
2023-10-01 16:15:00 +0700

    Fixed

--- deal-card-editor/static/scripts/index.js
+++ deal-card-editor/static/scripts/index.js
@@ -119,8 +119,8 @@ class App {
         this.elemBtnRewriteBottom.addEventListener("click", async (e) => {
             let spinner = this.elemBtnRewriteBottom.querySelector("span");
             spinner.classList.remove("d-none");
-            await this.handleSaveDealData();
             await this.getDataFromBx24();
+            await this.handleSaveDealData();
             await this.handleUpdateTask();
             spinner.classList.add("d-none");
         })
